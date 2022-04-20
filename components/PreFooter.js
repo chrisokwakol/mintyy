67dbacd678297e48d23fa86751e706ae0bbaa4f5
chris okwakol <chrisokwakol@gmail.com>
2022-04-20 13:19:06 +0300

    increased pitch deck card size on 375px screens

--- components/PreFooter.js
+++ components/PreFooter.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import {
+	faRocket,
+	faCaretSquareRight,
+} from "@fortawesome/free-regular-svg-icons";
 import styles from "../styles/PreFooter.module.css";
 
 function PreFooter() {
@@ -12,10 +15,14 @@ function PreFooter() {
 					Get Seen With Us and Take Your Project to the Next Level Now to See
 					Results!
 				</h4>
-				<a>Arrange a Meeting</a>
+				<a>Arrange a Meeting</a>{" "}
+				{/* <FontAwesomeIcon
+					icon={faCaretSquareRight}
+					className={styles.whyMintyCTAIcon}
+				/>{" "} */}
 			</div>
 		</div>
 	);
 }
- 
+
 export default PreFooter;
